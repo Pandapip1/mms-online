7cfa79f7c131c8f10681e2603690528c2803499a
Pandapip1 <45835846+Pandapip1@users.noreply.github.com>
2021-06-07 13:44:26 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,6 +2,11 @@ var app = new Vue({
   el: '#app',
   data: {
     signedin: false,
-    darkmode: false
+    darkmode: false,
+    numbers: [
+      '1234567890',
+      '2365543457',
+      '2435665786'
+    ]
   }
 });
