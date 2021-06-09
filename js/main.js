1335682b563c515ba9987cc21f20cf403c8bcaca
Pandapip1 <45835846+Pandapip1@users.noreply.github.com>
2021-06-09 12:04:04 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,6 +7,56 @@ var app = new Vue({
       '1234567890',
       '2365543457',
       '2435665786'
+    ],
+    messages: [
+      {
+        mine: false,
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus blandit, risus vel aliquam vehicula, lorem dui scelerisque nibh, et iaculis justo odio non lacus. Orci."
+      },
+      {
+        mine: false,
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus blandit, risus vel aliquam vehicula, lorem dui scelerisque nibh, et iaculis justo odio non lacus. Orci."
+      },
+      
+      {
+        mine: true,
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus blandit, risus vel aliquam vehicula, lorem dui scelerisque nibh, et iaculis justo odio non lacus. Orci."
+      },
+      
+      {
+        mine: false,
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus blandit, risus vel aliquam vehicula, lorem dui scelerisque nibh, et iaculis justo odio non lacus. Orci."
+      },
+      
+      {
+        mine: true,
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus blandit, risus vel aliquam vehicula, lorem dui scelerisque nibh, et iaculis justo odio non lacus. Orci."
+      },
+      
+      {
+        mine: true,
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus blandit, risus vel aliquam vehicula, lorem dui scelerisque nibh, et iaculis justo odio non lacus. Orci."
+      },
+      
+      {
+        mine: false,
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus blandit, risus vel aliquam vehicula, lorem dui scelerisque nibh, et iaculis justo odio non lacus. Orci."
+      },
+      
+      {
+        mine: true,
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus blandit, risus vel aliquam vehicula, lorem dui scelerisque nibh, et iaculis justo odio non lacus. Orci."
+      },
+      
+      {
+        mine: false,
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus blandit, risus vel aliquam vehicula, lorem dui scelerisque nibh, et iaculis justo odio non lacus. Orci."
+      },
+      
+      {
+        mine: true,
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus blandit, risus vel aliquam vehicula, lorem dui scelerisque nibh, et iaculis justo odio non lacus. Orci."
+      },
     ]
   }
 });
