72ad4816aa3f8c916555ae5b5460486b7159fb81
Pandapip1 <45835846+Pandapip1@users.noreply.github.com>
2021-06-09 12:24:09 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,60 +3,21 @@ var app = new Vue({
   data: {
     signedin: true,
     darkmode: false,
-    numbers: [
-      '1234567890',
-      '2365543457',
-      '2435665786'
-    ],
-    messages: [
-      {
-        mine: false,
-        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus blandit, risus vel aliquam vehicula, lorem dui scelerisque nibh, et iaculis justo odio non lacus. Orci."
-      },
-      {
-        mine: false,
-        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus blandit, risus vel aliquam vehicula, lorem dui scelerisque nibh, et iaculis justo odio non lacus. Orci."
-      },
-      
-      {
-        mine: true,
-        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus blandit, risus vel aliquam vehicula, lorem dui scelerisque nibh, et iaculis justo odio non lacus. Orci."
-      },
-      
-      {
-        mine: false,
-        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus blandit, risus vel aliquam vehicula, lorem dui scelerisque nibh, et iaculis justo odio non lacus. Orci."
-      },
-      
-      {
-        mine: true,
-        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus blandit, risus vel aliquam vehicula, lorem dui scelerisque nibh, et iaculis justo odio non lacus. Orci."
-      },
-      
-      {
-        mine: true,
-        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus blandit, risus vel aliquam vehicula, lorem dui scelerisque nibh, et iaculis justo odio non lacus. Orci."
-      },
-      
-      {
-        mine: false,
-        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus blandit, risus vel aliquam vehicula, lorem dui scelerisque nibh, et iaculis justo odio non lacus. Orci."
-      },
-      
-      {
-        mine: true,
-        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus blandit, risus vel aliquam vehicula, lorem dui scelerisque nibh, et iaculis justo odio non lacus. Orci."
-      },
-      
-      {
-        mine: false,
-        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus blandit, risus vel aliquam vehicula, lorem dui scelerisque nibh, et iaculis justo odio non lacus. Orci."
-      },
-      
-      {
-        mine: true,
-        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus blandit, risus vel aliquam vehicula, lorem dui scelerisque nibh, et iaculis justo odio non lacus. Orci."
-      },
-    ]
+    numbers: [],
+    messages: [],
+    newphone: '',
+    selectednum: '',
+    clearAddPhoneNum: () => {
+      app.newphone = '';
+    },
+    selectFirstPhoneNum: () => {
+      app.selectednum = app.numbers.length ? app.numbers[0] : '';
+      if (!app.numbers.length) document.getElementById("add-phone-num-btn").click();
+    },
+    addPhoneNum: () => {
+      app.numbers.push(app.newphone);
+      app.selectednum = app.newphone;
+      app.newphone = '';
+    }
   }
 });
